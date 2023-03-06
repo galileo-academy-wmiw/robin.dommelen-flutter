0e6a41aca47b4075ee790d213d7ab99058521542
Robin <robinvd95@live.nl>
2023-03-06 16:01:02 +0100

    push: 4.5

--- hello-world/lib/main.dart
+++ hello-world/lib/main.dart
@@ -25,7 +25,76 @@ class AppTree extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text("Content");
+    return ListView(
+      children: [
+        Container(
+          height: 2500,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+
+            children: [
+              Stack(
+                children: [
+                  Container(
+                    color: Colors.yellow,
+                    width: 100, height: 100,
+                  ),
+                  Positioned(
+                    left: 25,
+                    top: 40,
+                    child: Container(
+                      color: Colors.white.withOpacity(0.5),
+                      width: 50, height: 20,
+                    ),
+                  )
+                ],
+              ),
+              Expanded(
+                  flex: 2,
+                  child: Container(
+                      color: Colors.red,
+                      child: Center(
+                          child: Text("1")
+                      )
+                  )
+              ),
+              Expanded(
+                  child:
+                  Container(
+                    color: Colors.green,
+                    child: Center(
+                        child: Text("2")
+                    ),
+                  )
+              ),
+              Expanded(
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Container(
+                            color: Colors.blue,
+                            child: Center(
+                                child: Text("3")
+                            )
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(
+                            color: Colors.purple,
+                            child: Center(
+                                child: Text("4")
+                            )
+                        ),
+                      ),
+                    ],
+                  )
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
   }
 
 }
