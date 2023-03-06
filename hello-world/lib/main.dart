649ca745fa4a9492d24577f7073715815b94bceb
Robin <robinvd95@live.nl>
2023-03-06 16:19:06 +0100

    push: H4.6

--- hello-world/lib/main.dart
+++ hello-world/lib/main.dart
@@ -10,91 +10,59 @@ class AppRoot extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData.light(),
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(title: const Text("Hello World App!")),
-        body: Center(child: AppTree())
+      home: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(title: const Text("Hello World App!")),
+          body: TabBarView(
+            children: [
+              ScreenRed(), ScreenGreen(), ScreenBlue()
+            ],
+          ),
+          bottomNavigationBar: TabBar(
+            labelColor: Colors.black,
+            tabs: [
+              Tab(icon: Icon(Icons.home), text: "Red",),
+              Tab(icon: Icon(Icons.abc), text: "Green"),
+              Tab(icon: Icon(Icons.access_alarm), text: "Blue")
+            ]
+          ),
+        ),
       ),
     );
   }
 
 }
 
-class AppTree extends StatelessWidget {
-  const AppTree({super.key});
+class ScreenRed extends StatelessWidget {
+  const ScreenRed({super.key});
 
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        Container(
-          height: 2500,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
+    return Container(color: Colors.red,);
+  }
 
-            children: [
-              Stack(
-                children: [
-                  Container(
-                    color: Colors.yellow,
-                    width: 100, height: 100,
-                  ),
-                  Positioned(
-                    left: 25,
-                    top: 40,
-                    child: Container(
-                      color: Colors.white.withOpacity(0.5),
-                      width: 50, height: 20,
-                    ),
-                  )
-                ],
-              ),
-              Expanded(
-                  flex: 2,
-                  child: Container(
-                      color: Colors.red,
-                      child: Center(
-                          child: Text("1")
-                      )
-                  )
-              ),
-              Expanded(
-                  child:
-                  Container(
-                    color: Colors.green,
-                    child: Center(
-                        child: Text("2")
-                    ),
-                  )
-              ),
-              Expanded(
-                  child: Row(
-                    children: [
-                      Expanded(
-                        child: Container(
-                            color: Colors.blue,
-                            child: Center(
-                                child: Text("3")
-                            )
-                        ),
-                      ),
-                      Expanded(
-                        child: Container(
-                            color: Colors.purple,
-                            child: Center(
-                                child: Text("4")
-                            )
-                        ),
-                      ),
-                    ],
-                  )
-              ),
-            ],
-          ),
-        ),
-      ],
-    );
+}
+
+class ScreenBlue extends StatelessWidget {
+  const ScreenBlue({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(color: Colors.blue,);
+  }
+
+}
+
+class ScreenGreen extends StatelessWidget {
+  const ScreenGreen({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(color: Colors.green,);
   }
 
 }
